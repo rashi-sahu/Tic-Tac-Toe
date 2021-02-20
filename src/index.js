27b44b368fd47db07bd4b5be51b85730f6f46ac0
rashi-sahu <rashi.sahu13@gmail.com>
2021-02-20 23:51:15 +0530

    Changed Square to a function component

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-  class Square extends React.Component {
-    render() {
+  function Square(props) {
       return (
-        <button className="square" onClick={()=>this.props.onClick()}>
-          {this.props.value}
+        <button className="square" onClick={()=>props.onClick()}>
+          {props.value}
         </button>
       );
-    }
   }
   
   class Board extends React.Component {
@@ -22,7 +20,7 @@ import './index.css';
 
     handleClick(i){
       const sqaurescopy = this.state.squares.slice();
-      sqaurescopy[i] = 'X'
+      sqaurescopy[i] = 'X';
       this.setState({squares:sqaurescopy});
     }
 
